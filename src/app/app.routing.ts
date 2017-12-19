43e0057eb150deb89ef79a4339549df45b1e3431
Manuel <m.a.c.p90@gmail.com>
2017-12-19 12:40:16 -0500

    ride service

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 
 import { AuthComponent } from './auth/auth.component';
 import { CommentComponent } from './comment/comment.component';
-import { RideDetailsComponent } from './ride-details/ride-details.component';
 import {RideListComponent } from './ride-list/ride-list.component';
+import { RideDetailsComponent } from './ride-details/ride-details.component';
 
 export const routes: Routes = [
-    { path: 'api', component: AuthComponent },
-    { path: 'api', component: CommentComponent },
-    { path: 'api/rides', component: RideDetailsComponent },
+    { path: ' ', component: AuthComponent },
+    { path: ' ', component: CommentComponent },
+    { path: ' ', component: RideListComponent },
     { path: 'ride/:id', component: RideDetailsComponent },
     { path: '**', redirectTo: '' }
 ];
