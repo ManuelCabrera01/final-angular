2b0bace47160f7e931349c34116c3cc42e205c3d
Manuel <m.a.c.p90@gmail.com>
2017-12-18 19:47:59 -0500

    auth and comment component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RideListComponent } from './ride-list/ride-list.component';
 import { RideDetailsComponent } from './ride-details/ride-details.component';
+import { AuthComponent } from './auth/auth.component';
+import { CommentComponent } from './comment/comment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RideListComponent,
-    RideDetailsComponent
+    RideDetailsComponent,
+    AuthComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule
