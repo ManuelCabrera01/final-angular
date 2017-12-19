fa36b3fd91438bf6e0137a459058b3491edd444d
Manuel <m.a.c.p90@gmail.com>
2017-12-19 12:49:07 -0500

    servise implemted in ride-list componet.... not test have bin done yet

--- src/app/ride-list/ride-list.component.ts
+++ src/app/ride-list/ride-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import {RideService} from '../ride.service'
 @Component({
   selector: 'app-ride-list',
   templateUrl: './ride-list.component.html',
-  styleUrls: ['./ride-list.component.css']
+  styleUrls: ['./ride-list.component.css'],
+   providers: [RideService]
 })
 export class RideListComponent implements OnInit {
 
